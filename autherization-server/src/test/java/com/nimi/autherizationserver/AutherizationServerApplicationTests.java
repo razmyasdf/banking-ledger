740f82fa6db512e2598d129c43875a4fd6f4cc6f
Razmy Mahsoom <razmy.developer@hotelshippo.com>
2021-10-30 12:57:00 +0530

    Authentication server initial commit

---
+++ autherization-server/src/test/java/com/nimi/autherizationserver/AutherizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimi.autherizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutherizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
